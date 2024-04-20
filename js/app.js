48b897a3da4d8ce6a91e4c63906b54935dd0532b
Natanael Borges de Oliveira <naeelbass@gmail.com>
2024-04-19 21:44:22 -0300

    carrinho de compras

---
+++ js/app.js
@@ -0,0 +1,42 @@
+let valorTotal = 0;
+document.getElementById('quantidade').focus();
+function adicionar() {
+    document.getElementById('quantidade').focus();
+    
+    //recuperar valores nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valor = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    
+    
+    //calcular o preço, o nosso subtotal 
+    let preço = valor * quantidade;
+    
+    
+    //adicionar no carrinho 
+    let listaProdutos = document.getElementById('lista-produtos');
+    listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preço}</span>
+  </section>`;
+  limparQuantidade();
+    
+  //atualizar o valor total
+  valorTotal = valorTotal + preço;
+  let campoDoValorTotal = document.getElementById('valor-total');
+  campoDoValorTotal.textContent = `R$${valorTotal}`;
+  
+ }
+    
+ function limparQuantidade(quantidade) {
+    quantidade = document.getElementById('quantidade');
+    quantidade.value = '';
+    document.getElementById('quantidade').focus();
+ } 
+    function limparCarrinho() {
+        document.getElementById('lista-produtos').innerHTML = '';
+        document.getElementById('valor-total').textContent = 'R$ ';
+
+    
+    }
+    
